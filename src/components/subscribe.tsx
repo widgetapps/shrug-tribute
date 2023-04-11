322fcd80182a7f553083ab5ad1f4d9db593c70b2
Darryl Patterson <widgetapps@gmail.com>
2023-04-11 15:25:15 -0400

    Added the form.

--- src/components/subscribe.tsx
+++ src/components/subscribe.tsx
@@ -1,6 +1,7 @@
 import {Fragment} from "react";
 import {Dialog, Transition} from "@headlessui/react";
 import {XMarkIcon} from "@heroicons/react/24/outline";
+import {EnvelopeIcon} from "@heroicons/react/20/solid";
 
 // @ts-ignore
 export default function Subscribe({open, setOpen}) {
@@ -33,16 +34,16 @@ export default function Subscribe({open, setOpen}) {
                                 leaveTo="translate-x-full"
                             >
                                 <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                                        <div className="px-4 sm:px-6">
+                                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                                        <div className="bg-shrug-red px-4 py-6 sm:px-6">
                                             <div className="flex items-start justify-between">
-                                                <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                                                    Panel title
+                                                <Dialog.Title className="text-base font-semibold leading-6 text-white">
+                                                    GET ON THE ADVANCED TICKET LIST
                                                 </Dialog.Title>
                                                 <div className="ml-3 flex h-7 items-center">
                                                     <button
                                                         type="button"
-                                                        className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                        className="rounded-md text-white hover:text-gray-500 focus:outline-none focus:ring-1 focus:shrug-red-400 focus:ring-offset-1"
                                                         onClick={() => setOpen(false)}
                                                     >
                                                         <span className="sr-only">Close panel</span>
@@ -51,7 +52,102 @@ export default function Subscribe({open, setOpen}) {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="relative mt-6 flex-1 px-4 sm:px-6">{/* Your content */}</div>
+                                        <div className="relative mt-6 flex-1 px-4 sm:px-6 text-sm space-y-4">
+                                            <p>Get first access to ticket sales, just provide your email address and you’ll
+                                                receive an opportunity to purchase tickets before they go on sale for the
+                                                general public.</p>
+                                            <p>You’ll also have access to event merchandise like t-shirts and more!</p>
+                                            <p>Be sure to follow us on Facebook or Instagram.</p>
+
+                                            <div className="pb-8 space-y-4">
+                                                <div>
+                                                    <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                                        Name
+                                                    </label>
+                                                    <div className="mt-2">
+                                                        <input
+                                                            type="text"
+                                                            name="name"
+                                                            id="name"
+                                                            className="block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                            placeholder="Name"
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                <div>
+                                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                                        Email (required)
+                                                    </label>
+                                                    <div className="relative mt-2 rounded-md shadow-sm">
+                                                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                                            <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                        </div>
+                                                        <input
+                                                            type="email"
+                                                            name="email"
+                                                            id="email"
+                                                            className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                            placeholder="you@example.com"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <fieldset>
+                                                <legend className="sr-only">Opt-ins</legend>
+                                                <div className="space-y-5">
+                                                    <div className="relative flex items-start">
+                                                        <div className="flex h-6 items-center">
+                                                            <input
+                                                                id="shirt"
+                                                                aria-describedby="shirt-description"
+                                                                name="shirt"
+                                                                type="checkbox"
+                                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                                            />
+                                                        </div>
+                                                        <div className="ml-3 text-sm leading-6">
+                                                            <label htmlFor="shirt" className="font-medium text-gray-900">
+                                                                I'm interested in buying a shirt
+                                                            </label>
+                                                            <p id="shirt-description" className="text-gray-500">
+                                                                Final price is TBD, but should be $20-$30 each
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="relative flex items-start">
+                                                        <div className="flex h-6 items-center">
+                                                            <input
+                                                                id="subscribe"
+                                                                aria-describedby="subscribe-description"
+                                                                name="subscribe"
+                                                                type="checkbox"
+                                                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                                            />
+                                                        </div>
+                                                        <div className="ml-3 text-sm leading-6">
+                                                            <label htmlFor="subscribe" className="font-medium text-gray-900">
+                                                                Keep me updated on this event
+                                                            </label>
+                                                            <p id="subscribe-description" className="text-gray-500">
+                                                                You will be emailed updates for only this event
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </fieldset>
+
+                                            <div className="pt-4 justify-center items-center text-center">
+                                                <button
+                                                    type="button"
+                                                    className="rounded-md bg-shrug-red px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-shrug-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                                >
+                                                    ADD ME TO THE LIST
+                                                </button>
+                                            </div>
+
+                                        </div>
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,50 +14,50 @@ export default function Home() {
             <Subscribe
             open={open}
             setOpen={setOpen} />
-        <main className="flex min-h-screen flex-col items-center justify-between bg-photo-elephant bg-top-elephant sm:bg-top-elephant-sm bg-cover-elephant sm:bg-cover-elephant-md md:bg-cover bg-no-repeat">
-            <div className="w-full pt-14 flex flex-col items-center justify-center">
-                <div className="relative w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 rounded-full bg-white flex justify-center items-center bg-opacity-80 border-4 border-white">
-                    <Image
-                        src="/img/logo_shrug.svg"
-                        alt="Shrug Logo"
-                        className="w-48 h-48 -mt-5 sm:w-56 sm:-mt-7 md:w-64 md:-mt-9 lg:w-72 lg:-mt-10"
-                        width={400}
-                        height={400}
-                        priority
-                    />
-                </div>
-                <div className="mt-10 sm:mt-40 pt-4 pr-6 pb-6 pl-6 bg-shrug-bg text-center bg-opacity-80 sm:rounded-lg sm:shadow-md">
-                    <span className={`${ebgaramond.className} text-2xl md:text-3xl font-bold`}>
-                        A CELEBRATION of Mike Robillard’s Life in Music
-                    </span>
-                    <div className="grid grid-cols-2 sm:pl-8 sm:pr-8 pt-2 pb-1 mb-4">
-                        <div className="text-left">
-                            <h3 className={`${inter.className} text-xs sm:text-sm`}>Featuring performances by:</h3>
-                            <ul className="text-left text-sm sm:text-base">
-                                <li>4 Really Nice Guys</li>
-                                <li>Scissors For Erica</li>
-                                <li>Spiderback</li>
-                            </ul>
-                        </div>
-                        <div className="text-right">
-                            <h3 className={`${inter.className} text-xs sm:text-sm`}>One night only:</h3>
-                            <ul className="text-right text-sm sm:text-base">
-                                <li>Thursday, Nov 2, 2023</li>
-                                <li>Loplops Gallery Lounge</li>
-                                <li>Sault Ste Marie, ON</li>
-                            </ul>
-                        </div>
+            <main className="flex min-h-screen flex-col items-center justify-between bg-photo-elephant bg-top-elephant sm:bg-top-elephant-sm bg-cover-elephant sm:bg-cover-elephant-md md:bg-cover bg-no-repeat">
+                <div className="w-full pt-14 flex flex-col items-center justify-center">
+                    <div className="relative w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 rounded-full bg-white flex justify-center items-center bg-opacity-80 border-4 border-white">
+                        <Image
+                            src="/img/logo_shrug.svg"
+                            alt="Shrug Off Reality Presents"
+                            className="w-48 h-48 -mt-5 sm:w-56 sm:-mt-7 md:w-64 md:-mt-9 lg:w-72 lg:-mt-10"
+                            width={400}
+                            height={400}
+                            priority
+                        />
                     </div>
-                    <button
-                        type="button"
-                        onClick={() => setOpen(true)}
-                        className="rounded-md bg-shrug-red px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-shrug-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        GET ON THE ADVANCED TICKET LIST
-                    </button>
+                    <div className="mt-10 sm:mt-40 pt-4 pr-6 pb-6 pl-6 bg-shrug-bg text-center bg-opacity-80 sm:rounded-lg sm:shadow-md">
+                        <span className={`${ebgaramond.className} text-2xl md:text-3xl font-bold`}>
+                            A CELEBRATION of Mike Robillard’s Life in Music
+                        </span>
+                        <div className="grid grid-cols-2 sm:pl-8 sm:pr-8 pt-2 pb-1 mb-4">
+                            <div className="text-left">
+                                <h3 className={`${inter.className} text-xs sm:text-sm`}>Featuring performances by:</h3>
+                                <ul className="text-left text-sm sm:text-base">
+                                    <li>4 Really Nice Guys</li>
+                                    <li>Scissors For Erica</li>
+                                    <li>Spiderback</li>
+                                </ul>
+                            </div>
+                            <div className="text-right">
+                                <h3 className={`${inter.className} text-xs sm:text-sm`}>One night only:</h3>
+                                <ul className="text-right text-sm sm:text-base">
+                                    <li>Thursday, Nov 2, 2023</li>
+                                    <li>Loplops Gallery Lounge</li>
+                                    <li>Sault Ste Marie, ON</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <button
+                            type="button"
+                            onClick={() => setOpen(true)}
+                            className="rounded-md bg-shrug-red px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-shrug-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            GET ON THE ADVANCED TICKET LIST
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
         </div>
     )
 }
