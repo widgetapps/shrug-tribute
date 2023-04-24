ee59c11fbb6b0b4c27e2a1b434d5c265776e3215
Darryl Patterson <widgetapps@gmail.com>
2023-04-24 15:30:17 -0400

    Added the database name to ENV. Added date to the subscribe.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -4,14 +4,15 @@ import {NextApiRequest, NextApiResponse} from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const client = await clientPromise;
-        const db = client.db('shrug-dev');
+        const db = client.db(process.env.DB_NAME);
         const { fullname, email, shirt, subscribe } = req.body;
 
         const post = await db.collection('subscribers').insertOne({
             fullname,
             email,
             shirt,
-            subscribe
+            subscribe,
+            'date': new Date()
         });
 
         res.json(post);
