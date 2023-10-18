a5dd41e4e75b9e742db793f294324f216df0bae9
Darryl Patterson <widgetapps@gmail.com>
2023-10-18 12:07:33 -0400

    Added title (duh) and the missing meta tags.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,33 @@ export default function Document() {
           `,
                 }}
             />
+
+            <title>Shrug off reality... 2023 - A Celebration of Mike Robillard's Life in Music</title>
+
+            <meta name="description" content="On November 2nd, current and former members of the local music community
+            will be holding a special event to celebrate the life and music of one of their own who succumbed to cancer
+            in 2022. Mike Robillard was a bass player, writer, artist, and podcaster, whose passing left behind a legacy
+            of passion for the arts." />
+
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content=">Shrug off reality... 2023 - A Celebration of Mike Robillard's Life in Music" />
+            <meta property="og:description" content="On November 2nd, current and former members of the local music community
+            will be holding a special event to celebrate the life and music of one of their own who succumbed to cancer
+            in 2022. Mike Robillard was a bass player, writer, artist, and podcaster, whose passing left behind a legacy
+            of passion for the arts." />
+            <meta property="og:image" content="https://www.shrugoffreality.com/_next/image?url=%2Fimg%2Fphoto_mike_1.png&w=640&q=75" />
+            <meta property="og:url" content="https://www.shrugoffreality.com/" />
+            <meta property="og:site_name" content="Shrug off reality..." />
+
+            <meta name="twitter:title" content=">Shrug off reality... 2023 - A Celebration of Mike Robillard's Life in Music" />
+            <meta name="twitter:description" content="On November 2nd, current and former members of the local music community
+            will be holding a special event to celebrate the life and music of one of their own who succumbed to cancer
+            in 2022. Mike Robillard was a bass player, writer, artist, and podcaster, whose passing left behind a legacy
+            of passion for the arts." />
+            <meta name="twitter:image" content="https://www.shrugoffreality.com/_next/image?url=%2Fimg%2Fphoto_mike_1.png&w=640&q=75" />
+            <meta name="twitter:url" content="https://www.shrugoffreality.com/" />
+
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
         </Head>
       <body className="bg-black">
         <Mainnav />
